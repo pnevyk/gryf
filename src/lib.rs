274dfafcb63706ea958fa1403cbcb77b32a068d8
Petr Nevyhoštěný <petr.nevyhosteny@gmail.com>
2022-02-13 15:04:21 +0100

    Update unstable features list for rustc 1.60.0 nightly

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,5 @@
-#![allow(incomplete_features)]
 #![feature(generic_associated_types)]
 #![feature(associated_type_defaults)]
-#![feature(maybe_uninit_extra)]
 #![feature(assert_matches)]
 #![feature(int_log)]
 #![feature(try_blocks)]

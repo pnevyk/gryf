d77d709ac9ad048ecdfb16a8a853db02d878dcc5
Petr Nevyhoštěný <petr.nevyhosteny@gmail.com>
2023-05-16 23:19:43 +0200

    Add algoritm for checking connectedness of a graph/vertices
    This mainly serves as a precedent for algorithms with our standard
    structure (`on(graph)` constructor, builder pattern, result struct),
    which may also have "aliases" in form of a standalone function with
    simpler input and simpler output.

---
+++ gryf/src/algo/connected/builder.rs
@@ -0,0 +1,56 @@
+use crate::core::{marker::EdgeType, EdgesBase, GraphBase, Neighbors, VerticesBase};
+
+use super::{dfs::dfs, Connected};
+
+pub struct ConnectedBuilder<'a, G>
+where
+    G: GraphBase,
+{
+    graph: &'a G,
+    between: Option<(&'a G::VertexIndex, &'a G::VertexIndex)>,
+    as_undirected: bool,
+}
+
+impl<G> Connected<G>
+where
+    G: GraphBase,
+{
+    pub fn on(graph: &G) -> ConnectedBuilder<'_, G> {
+        ConnectedBuilder {
+            graph,
+            between: None,
+            as_undirected: false,
+        }
+    }
+}
+
+impl<'a, G> ConnectedBuilder<'a, G>
+where
+    G: GraphBase,
+{
+    pub fn between(self, src: &'a G::VertexIndex, dst: &'a G::VertexIndex) -> Self {
+        Self {
+            between: Some((src, dst)),
+            ..self
+        }
+    }
+
+    pub fn as_undirected(self) -> Self {
+        Self {
+            as_undirected: true,
+            ..self
+        }
+    }
+}
+
+impl<'a, G> ConnectedBuilder<'a, G>
+where
+    G: GraphBase,
+{
+    pub fn run<Ty: EdgeType>(self) -> Connected<G>
+    where
+        G: Neighbors + VerticesBase + EdgesBase<Ty>,
+    {
+        dfs(self.graph, self.between, self.as_undirected)
+    }
+}

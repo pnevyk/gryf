6896b9e4347dc8fe9945f7c7ef78cf2459e0391b
Petr Nevyhoštěný <petr.nevyhosteny@gmail.com>
2024-08-08 09:52:19 +0200

    Implement core traits for graph references
    Ideally, an automatic mechanism would be used like implemented by the
    auto_impl crate. However, at this moment, gryf runs into
    https://github.com/auto-impl-rs/auto_impl/issues/87

--- gryf/src/core/props.rs
+++ gryf/src/core/props.rs
@@ -67,3 +67,42 @@ mod private {
     impl Sealed for NoReplace {}
     impl Sealed for ReplaceRemoved {}
 }
+
+mod imp {
+    use super::{Guarantee, MultiEdge};
+
+    impl<G> MultiEdge for &G where G: MultiEdge {}
+    impl<G> MultiEdge for &mut G where G: MultiEdge {}
+
+    macro_rules! deref_guarantee {
+        ($($ref_kind:tt)*) => {
+            impl<G> Guarantee for $($ref_kind)* G
+            where
+                G: Guarantee,
+            {
+                fn is_loop_free() -> bool {
+                    G::is_loop_free()
+                }
+
+                fn has_paths_only() -> bool {
+                    G::has_paths_only()
+                }
+
+                fn has_trees_only() -> bool {
+                    G::has_trees_only()
+                }
+
+                fn has_bipartite_only() -> bool {
+                    G::has_bipartite_only()
+                }
+
+                fn is_connected() -> bool {
+                    G::is_connected()
+                }
+            }
+        }
+    }
+
+    deref_guarantee!(&);
+    deref_guarantee!(&mut );
+}

9d850f8c787846a6316c5036257d80117cc3308c
Petr Nevyhoštěný <petr.nevyhosteny@gmail.com>
2024-08-11 17:35:11 +0200

    Remove ExtendWithEdges::from_edges
    Without extend_with_edges auto-creating the vertices, this function does
    not make sense. Instead, the expected approach is to create a graph
    using from_vertices and then add edges using extend_with_edges.

--- gryf/src/core/create.rs
+++ gryf/src/core/create.rs
@@ -1,5 +1,3 @@
-use std::cmp::max;
-
 use super::{base::IntoEdge, graph::GraphAdd};
 
 pub trait Create<V, E>: GraphAdd<V, E> + Sized {
@@ -17,19 +15,6 @@ where
     fn extend_with_edges<I>(&mut self, iter: I)
     where
         I: IntoIterator<Item = T>;
-
-    fn from_edges<I>(iter: I) -> Self
-    where
-        I: IntoIterator<Item = T>,
-    {
-        let iter = iter.into_iter();
-        let edge_count = iter.size_hint().1.unwrap_or(32);
-        let vertex_count = max(edge_count / 4, 2);
-
-        let mut graph = Self::with_capacity(vertex_count, edge_count);
-        graph.extend_with_edges(iter);
-        graph
-    }
 }
 
 impl<T, V, E, G> ExtendWithEdges<T, V, E> for G

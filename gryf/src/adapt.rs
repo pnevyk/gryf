6af11672b53081b7edede53ba70b23ded4e49a39
Petr Nevyhoštěný <petr.nevyhosteny@gmail.com>
2024-08-15 13:47:38 +0200

    Make the subgraph adapter public

--- gryf/src/adapt.rs
+++ gryf/src/adapt.rs
@@ -1,11 +1,18 @@
-#![doc(hidden)]
-
+#[doc(hidden)]
 pub mod complement;
-pub mod subgraph;
+#[doc(hidden)]
 pub mod transpose;
+#[doc(hidden)]
 pub mod undirect;
 
+pub mod subgraph;
+
+#[doc(hidden)]
 pub use complement::Complement;
-pub use subgraph::Subgraph;
+#[doc(hidden)]
 pub use transpose::Transpose;
+#[doc(hidden)]
 pub use undirect::Undirect;
+
+#[doc(inline)]
+pub use subgraph::Subgraph;
